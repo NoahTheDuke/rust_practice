f87dfb2d1e537ac868426f6db4636a6fc61d6522
Noah Bogart <nbtheduke@gmail.com>
2016-07-06 02:18:16 -0400

    Strand Sort finished.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn sort(mut unsorted: Vec<i32>) -> Vec<i32> {
     if unsorted.len() < 2 {
         return unsorted
     }
-    let mut result = Vec::new();
+    let mut result = Vec::with_capacity(unsorted.len());
     while unsorted.len() > 0 {
         let mut sublist = vec![unsorted.remove(0)];
         let mut leftovers = Vec::new();
@@ -16,15 +16,55 @@ fn sort(mut unsorted: Vec<i32>) -> Vec<i32> {
                 leftovers.push(item);
             }
         }
-        println!("result: {:?}", result);
-        println!("sublis: {:?}", sublist);
-        result.extend(sublist);
+        result = merge(&result, &sublist);
         unsorted = leftovers;
     }
     result
 }
 
+fn merge2(left: &Vec<i32>, right: &Vec<i32>) -> Vec<i32> {
+    if left.len() == 0 {
+        return right.clone()
+    } else if right.len() == 0 {
+        return left.clone()
+    }
+
+    let mut result: Vec<i32> = Vec::with_capacity(left.len() + right.len());
+    let offset = left.len();
+    result.extend(left);
+    result.extend(right);
+    result
+}
+
+fn merge(left: &Vec<i32>, right: &Vec<i32>) -> Vec<i32> {
+    if left.len() == 0 {
+        return right.clone()
+    } else if right.len() == 0 {
+        return left.clone()
+    }
+
+    let mut result: Vec<i32> = Vec::with_capacity(left.len() + right.len());
+    let mut a_dx: usize = 0;
+    let mut b_dx: usize = 0;
+    while a_dx < left.len() && b_dx < right.len() {
+        if left[a_dx] <= right[b_dx] {
+            result.push(left[a_dx].clone());
+            a_dx += 1;
+        } else {
+            result.push(right[b_dx].clone());
+            b_dx += 1;
+        }
+    }
+
+    if a_dx < left.len() {
+        result.extend(left[a_dx..].iter());
+    }
+    if b_dx < right.len() {
+        result.extend(right[b_dx..].iter());
+    }
+    result
+}
+
 fn main() {
-    println!("{:?}", sort(vec![2, 1, 3]));
-    // println!("{:?}", sort(vec![1]));
+    println!("{:?}", sort(vec![2, 1, 3, 4, 6, 8, 10, 9, 7, 5, 11]));
 }
